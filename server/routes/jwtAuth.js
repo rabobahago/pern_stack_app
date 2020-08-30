ad9bf7b37efe440f43ab281de298588c6de95503
rabobahago <rabobahago@gmail.com>
2020-08-30 19:24:40 +0100

    save and get users from database

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const pool = require('../db')
+const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res) => {
   try {
@@ -10,11 +11,19 @@ router.post('/register', async (req, res) => {
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
       email,
     ])
-    res.json(user.rows)
+    if (user.rows.length !== 0) {
+      return res.status(401).send('User already exists')
+    }
     //3) Bcrypt the user password
-
+    const saltRound = 10
+    const salt = await bcrypt.genSalt(saltRound)
+    const hashPassword = await bcrypt.hash(password, salt)
     //4) Enter the new user into our database
-
+    const newUser = await pool.query(
+      'INSERT INTO users(user_name, user_email, user_password) VALUES($1, $2, $3) RETURNING *',
+      [name, email, hashPassword],
+    )
+    res.json(newUser.rows[0])
     //5) Generate token
   } catch (error) {
     console.error(error.message)
