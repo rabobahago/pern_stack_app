092a5883f362ba3f9b1717f8dd93f6d964839bbd
rabobahago <rabobahago@gmail.com>
2020-08-31 12:57:27 +0100

    sign in route completed

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -33,4 +33,32 @@ router.post('/register', async (req, res) => {
   }
 })
 
+//Login Route
+router.post('/login', async (req, res) => {
+  try {
+    // 1) destructure req.body
+    const { email, password } = req.body
+    //2) check if you user don't exist, if not throw error
+    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
+      email,
+    ])
+    if (user.rows.length === 0) {
+      return res.status(401).json('Incorrect password or email')
+    }
+    //3) check if incoming password is the same as database password
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password,
+    )
+    if (!validPassword) {
+      return res.status(401).send('Incorrect password or email')
+    }
+    //4)  give jwt token
+    const token = jwtGenerator(user.rows[0].user_id)
+    res.json({ token })
+  } catch (error) {
+    console.error(error.message)
+    res.status(401).send('server error')
+  }
+})
 module.exports = router
